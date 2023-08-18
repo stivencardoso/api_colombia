d443057090ba98993c9eeb922fed76d5f16f19fb
stivencardoso <jstiven0418@gmail.com>
2023-08-18 15:43:19 -0500

    api

---
+++ colombiapi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace colombiapi
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = "https://api-colombia.com/api/v1/Country/Colombia";
+                client.DefaultRequestHeaders.Clear();
+                var response = client.GetAsync(url).Result;
+                var res = response.Content.ReadAsStringAsync().Result;
+                dynamic r = res;
+
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
